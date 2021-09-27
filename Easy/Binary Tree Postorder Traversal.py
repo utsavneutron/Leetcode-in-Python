cb979f148ea5dacf2559d10e72472ad8dcff41c8
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2021-09-26 20:59:37 -0500

    Day 22

--- Easy/Binary Tree Postorder Traversal.py
+++ Easy/Binary Tree Postorder Traversal.py
@@ -2,9 +2,10 @@ def postOrderTraversal(self,root):
     res = []
 
     def postOrder(node):
-        postOrder(node.left)
-        postOrder(node.right)
-        res.append(node.val)
+        if node:
+            postOrder(node.left)
+            postOrder(node.right)
+            res.append(node.val)
 
     postOrder(root)
 
