ac4026d722f76176449efc1539386864af39edaa
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2021-09-26 20:53:47 -0500

    Day 22

---
+++ Easy/Binary Tree Inorder Traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root):
+        res = []
+
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                res.append(node.val)
+                inorder(node.right)
+
+        inorder(root)
+
+        return res
--- Easy/Binary Tree Preorder Traversal.py.py
+++ Easy/Binary Tree Preorder Traversal.py.py
@@ -1,16 +1,32 @@
-def preorderTraversal(self,root):
-    stack,res = [],[]
+# Recursive Approach
+def preorderTraversal(self,node):
+    res = []
 
-    if root:
-        stack.append(root)
-
-    while(stack):
-        root = stack.pop()
-        res.append(root.val)
-
-        if root.right:
-            stack.append(root.right)
-        if root.left:
-            stack.append(root.left)
+    def preorder(node):
+        res.append(node.val)
+        preorder(node.left)
+        preorder(node.right)
+    
+    preorder(root)
 
     return res
+
+
+
+# Iterative Approach
+# def preorderTraversal(self,root):
+#     stack,res = [],[]
+
+#     if root:
+#         stack.append(root)
+
+#     while(stack):
+#         root = stack.pop()
+#         res.append(root.val)
+
+#         if root.right:
+#             stack.append(root.right)
+#         if root.left:
+#             stack.append(root.left)
+
+#     return res
