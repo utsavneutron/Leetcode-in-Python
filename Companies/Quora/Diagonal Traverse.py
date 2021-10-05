6bf9fbb8945e6d8a49b347650ffc5060435c7303
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2021-10-05 14:54:24 -0500

    Quora #2

---
+++ Companies/Quora/Diagonal Traverse.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
+        d = {}
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i+j not in d:
+                    d[i+j] = [matrix[i][j]]
+                else:
+                    d[i+j].append(matrix[i][j])
+
+        ans = []
+
+        for key, val in d.items():
+            if key % 2 == 0:  # Entry[0] means key
+                [ans.append(x) for x in val[::-1]]
+            else:
+                [ans.append(x) for x in val]
+
+        return ans
