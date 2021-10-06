e5b809d349919b92830aefd161bee8e0b948351e
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2021-10-06 14:04:18 -0500

    Quora #3

---
+++ Companies/Quora/Previous Permutation with One Swap.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/previous-permutation-with-one-swap/
+
+def prevPermOpt(self,A):
+    i = len(A) - 2
+
+    while i >= 0 and A[i] <= A[i+1]:
+        i -= 1
+    
+    if i >= 0:
+        max_ = i+1
+        for j in range(max_+1, len(A)):
+            if A[max] < A[j] < A[i]:
+                max_ = j
+        
+        A[max_], A[i] = A[i], A[max_]
+
+    return A 
